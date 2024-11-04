dbcec12f1d73d1c1479c7e5aa98bdc3f1413daf5
mesue007 <mesue01@outlook.com>
2024-11-04 12:42:31 -0500

    Update interpreter.py

--- Week 1/interpreter/interpreter.py
+++ Week 1/interpreter/interpreter.py
@@ -1,1 +1,16 @@
+expression = input("Expression: ")
+x, y, z = expression.split(" ")
+a = int(x)
+b = int(z)
 
+if y == "+":
+    print(float(a+b))
+
+elif y == "-":
+    print(float(a-b))
+
+elif y == "*":
+    print(float(a*b))
+
+elif y == "/":
+    print(float(a/b))
