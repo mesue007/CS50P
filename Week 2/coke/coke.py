cf2552f55532ff87387bd47038baf57283dd36e8
mesue007 <mesue01@outlook.com>
2024-11-04 12:50:00 -0500

    Update coke.py

--- Week 2/coke/coke.py
+++ Week 2/coke/coke.py
@@ -1,1 +1,20 @@
+coin = int(input("Insert Coin: "))
 
+i = 50
+
+while i - coin > 0:
+    if coin == 25:
+        print(f"Amount Due: {i - 25}")
+    elif coin == 10:
+        print(f"Amount Due: {i - 10}")
+    elif coin == 5:
+        print(f"Amount Due: {i - 5}")
+    else:
+        print(f"Amount Due: {i}")
+    i = i - coin
+    coin = int(input("Insert Coin: "))
+
+if i - coin == 0:
+    print("Change Owed: 0")
+elif i - coin < 0:
+    print(f"Change Owed: {coin-i}")
