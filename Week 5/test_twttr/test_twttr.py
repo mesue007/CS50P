f387c070baf12de20e3d9013716f522f6e1148bf
mesue007 <mesue01@outlook.com>
2024-11-04 13:02:01 -0500

    Update test_twttr.py

--- Week 5/test_twttr/test_twttr.py
+++ Week 5/test_twttr/test_twttr.py
@@ -1,1 +1,21 @@
+from twttr import shorten
 
+def test_shorten():
+    assert shorten("twitter") == "twttr"
+    assert shorten("faizan") == "fzn"
+    assert shorten("test") == "tst"
+
+
+def test_uppercase():
+    assert shorten("FAIzAN") == "FzN"
+    assert shorten("tEST") == "tST"
+
+
+def test_numb():
+    assert shorten("FAIzAN1") == "FzN1"
+    assert shorten("3tEST") == "3tST"
+
+
+def test_punc():
+    assert shorten(".FAIzAN1") == ".FzN1"
+    assert shorten("1.twitter") == "1.twttr"
