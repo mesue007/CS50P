821353b73abd71bb5bfb49adbe04ea7c5c9a6d4a
mesue007 <mesue01@outlook.com>
2024-11-04 12:56:27 -0500

    Update adieu.py

--- Week 4/adieu/adieu.py
+++ Week 4/adieu/adieu.py
@@ -1,1 +1,22 @@
+import inflect
+p = inflect.engine()
+i = 0
+while True:
+    try:
+        person = input("Name: ")
+        if i > 0:
+            persons = (persons + " " + person)
+        else:
+            persons = person
+        i += 1
 
+
+    except EOFError:
+        try:
+            persons = persons.split()
+            print(f"Adieu, adieu, to {p.join(persons)}")
+            break
+        except NameError:
+            break
+    except KeyError:
+        pass
