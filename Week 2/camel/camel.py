c63a7044f02445ff0926432ddc8c8b959a2088f6
mesue007 <mesue01@outlook.com>
2024-11-04 12:49:25 -0500

    Update camel.py

--- Week 2/camel/camel.py
+++ Week 2/camel/camel.py
@@ -1,1 +1,9 @@
+cCase = input("camelCase: ")
 
+for c in cCase:
+    if c.isupper() == False:
+        print(c, end="")
+    elif c.isupper() == True:
+        print(f"_{c.lower()}", end="")
+
+print()
