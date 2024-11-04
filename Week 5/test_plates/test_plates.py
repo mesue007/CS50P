971be0bd00e7ce622772bbc772039854376f5a66
mesue007 <mesue01@outlook.com>
2024-11-04 13:01:29 -0500

    Update test_plates.py

--- Week 5/test_plates/test_plates.py
+++ Week 5/test_plates/test_plates.py
@@ -1,1 +1,9 @@
+from plates import is_valid
 
+def test_plate():
+    assert is_valid("CS50") == True
+    assert is_valid("CS05") == False
+    assert is_valid("CS50P") == False
+    assert is_valid("PI3.14") == False
+    assert is_valid("H") == False
+    assert is_valid("OUTATIME") == False
