de6e6031ed603d6fe46e88d3d182e0a8fc313a3d
mesue007 <mesue01@outlook.com>
2024-11-04 12:58:27 -0500

    Update game.py

--- Week 4/game/game.py
+++ Week 4/game/game.py
@@ -1,1 +1,36 @@
+import random
+def main():
+    while True:
+        n = input("Level: ")
+        if n.isdigit() == True:
+            if int(n) > 0:
+                guess(n)
+                break
 
+        else:
+            pass
+
+def guess(s):
+    a = random.randint(1, int(s))
+    while True:
+        int(a)
+        g = input("Guess: ")
+
+        if g.isdigit() == False:
+            pass
+
+        elif int(g) > int(s) or int(g) < 1:
+            pass
+
+        elif int(g) < a:
+            print("Too small!")
+
+        elif int(g) > a:
+            print("Too large!")
+
+        else:
+            print("Just right!")
+            break
+
+
+main()
