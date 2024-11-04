0672445479cb60581a23f8e9279fb96a3d95310a
mesue007 <mesue01@outlook.com>
2024-11-04 12:53:45 -0500

    Update grocery.py

--- Week 3/grocery/grocery.py
+++ Week 3/grocery/grocery.py
@@ -1,1 +1,23 @@
+i = 0
+while True:
+    try:
+        item = input().upper()
+        if i > 0:
+            items.append(item)
+        else:
+            items = [item]
+        i += 1
 
+
+    except EOFError:
+        try:
+            print()
+            items = sorted(items)
+            d = dict((x,items.count(x)) for x in set(items))
+            for key, value in d.items():
+                print(f"{value} {key}")
+            break
+        except NameError:
+            break
+    except KeyError:
+        pass
