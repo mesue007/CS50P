ef27bf3d49f54882f802194e311182ef598d7a4d
mesue007 <mesue01@outlook.com>
2024-11-04 12:56:54 -0500

    Update bitcoin.py

--- Week 4/bitcoin/bitcoin.py
+++ Week 4/bitcoin/bitcoin.py
@@ -1,1 +1,19 @@
+import sys
+import requests
+import json
 
+try:
+    if len(sys.argv) < 2:
+        sys.exit("Missing command-line argument")
+    elif sys.argv[1].isalpha() == True:
+        sys.exit("Command-line argument is not a number")
+    else:
+        response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+        #print(json.dumps(response.json(), indent=2))
+        data = response.json()
+        bpi_usd = data["bpi"]["USD"]
+        usd = bpi_usd["rate_float"]
+        amt = float(sys.argv[1])*float(usd)
+        print(f"${amt:,.4f}")
+except requests.RequestException:
+    pass
