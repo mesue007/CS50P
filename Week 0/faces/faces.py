747afdf8cf4a08e5c863a38cb4f55d1b73b58bd1
mesue007 <mesue01@outlook.com>
2024-11-04 12:04:48 -0500

    Update faces.py

--- Week 0/faces/faces.py
+++ Week 0/faces/faces.py
@@ -1,1 +1,8 @@
+def main():
+    text = input("Insert text plus emoticon to be converted: ")
+    convert(text)
 
+def convert(text):
+    print(text.replace(":)", "🙂").replace(":(", "🙁"))
+
+main()
