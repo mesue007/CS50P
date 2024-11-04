7f70d451a3dee3997d59cf866d82df616496d524
mesue007 <mesue01@outlook.com>
2024-11-04 12:41:31 -0500

    Update deep.py

--- Week 1/deep/deep.py
+++ Week 1/deep/deep.py
@@ -1,1 +1,16 @@
+def main():
+    answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
 
+    if answer == "42" or answer == "forty-two" or answer == "forty two":
+        print("Yes")
+    elif ignorespace(answer) == "42":
+        print("Yes")
+    elif answer.lower() == "forty two":
+        print("Yes")
+    else:
+        print("No")
+
+def ignorespace(text):
+    return text.replace(" ", "")
+
+main()
