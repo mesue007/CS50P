e081b3fd6a88c06173e5e4a04d6b79ffcf081419
mesue007 <mesue01@outlook.com>
2024-11-04 12:55:54 -0500

    Update taqueria.py

--- Week 3/taqueria/taqueria.py
+++ Week 3/taqueria/taqueria.py
@@ -1,1 +1,26 @@
+d = {
+    "baja taco": 4.25,
+    "burrito": 7.50,
+    "bowl": 8.50,
+    "nachos": 11.00,
+    "quesadilla": 8.50,
+    "super burrito": 8.50,
+    "super quesadilla": 9.50,
+    "taco": 3.00,
+    "tortilla salad": 8.00
+}
 
+total = 0
+while True:
+    try:
+        item = input("Item: ").lower()
+        for key in d:
+            if key == item:
+                total = float(d[item]) + float(total)
+                print(f"Total: ${total:.2f}")
+
+    except EOFError:
+        print()
+        break
+    except KeyError:
+        pass
