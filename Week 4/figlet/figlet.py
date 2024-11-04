ad36cbc3a9275c63ec37dd4b8e5162d59abac893
mesue007 <mesue01@outlook.com>
2024-11-04 12:57:55 -0500

    Update figlet.py

--- Week 4/figlet/figlet.py
+++ Week 4/figlet/figlet.py
@@ -1,1 +1,28 @@
+import sys
+from random import choice
+from pyfiglet import Figlet
 
+figlet = Figlet()
+
+figlet.getFonts()
+
+if sys.argv[1] != ("-f" or "--font"):
+    sys.exit("Please input -f or --font as first argument")
+
+elif len(sys.argv) == 3:
+    f = sys.argv[2]
+
+    figlet.setFont(font=f)
+
+    s = input("Input: ")
+
+    print(figlet.renderText(s))
+
+elif len(sys.argv) == 1:
+    f = choice(figlet.getFonts())
+
+    figlet.setFont(font=f)
+
+    s = input("Input: ")
+
+    print(figlet.renderText(s))
